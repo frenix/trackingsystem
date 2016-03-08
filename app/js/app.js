fa218624f6ea7582537526643be49bc3f9fd82d3
Efren Duran <efren.duranjr@ddp.kyocera.com>
2016-03-08 19:37:17 +0800

    app.js -> login logic is working as expected version 1

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,8 @@
+function log(header, details)
+{
+    //alert(header + ": " + details);
+}
+
 function setCookie(cname,cvalue,exdays) {
 
     console.log("<< setCookie");
@@ -32,30 +37,61 @@ function eraseCookie() {
 
 function checkCookie() {
     console.log("checkCookie()");
-    alert($('#userName').val());
-    //use username
+    var user;
+    var title = document.title;
+    //log("Document title ->" + title);
     var userName = $('#userName').val();
     var passWord = $('#passPhrase').val();
-    alert(userName);
-   
-    var user=getCookie(userName);
+
+     //get a predefined cookie, this cookie will hold the 'current' login user
+    var currentuser = getCookie("curuser");
+    log("curuser ", currentuser);
+    if (currentuser != "" && currentuser != null && userName == "" && userName == null) {
+        //if there is already user who successfully login
+        userName = currentuser;
+    }
+
+    //log("userName textfield: " + $('#userName').val());
+    //log("password textfield: " + $('#passPhrase').val());
+    //use username
+    
+    log("Username", userName);
+    
+    user=getCookie(userName);
     var password="";
-    alert("user: " + user);
+    log("user",  user);
 
-
+   
     if (user != "" || userName == "admin") {
-        alert("Welcome again " + user);
-        //window.location="adduser.html";
+        alert("Welcome again " + userName);
+
+        //set also to a currentuser cookie
+        setCookie("curuser", userName, 30);
         return true;
     } else {
        //user = prompt("Please enter your name:","");
-        alert("username is " + user);
+       // log("password is " + user);
 
-       if (user == "" && userName != "") {
-            alert(">> setCookie");
+       if (user == "" && userName != "" && title == "Add User") {
+            //log(">> setCookie");
             //setCookie("username", user+password, 30);
             setCookie(userName, passWord, 30);
-            alert("setCookie");
+
+            alert("User successfully saved!");
+       } else if (title != "Login") {
+            // check if page is not Login page
+            // getcookie and route to login page if cookie not found
+            if(currentuser == "") {
+                window.location = "login.html";
+            }
+       } else if (title == "Login") {
+            if(currentuser != "") {
+                //set curuser to empty
+                //imitates logout
+                setCookie("curuser", "", 30);
+            }
+       } else if (userName !="" && passWord !="") {
+            alert("User not found!");
        }
        return false;
     }
