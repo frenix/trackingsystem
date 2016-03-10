5ca886513f7837e99e9d2069b709b1886be111a1
Efren Duran <efren.duranjr@ddp.kyocera.com>
2016-03-10 17:29:22 +0800

    Updated static data

--- app/js/depositreport.js
+++ app/js/depositreport.js
@@ -1,9 +1,9 @@
 //dummy data
-var montharr = [[1, "<tr><td>1/29/2016</td><td>Jackson, Florida</td><td>94</td><td>Just nothing</td><td>9400</td><td>000-1111-222</td></tr>"],
-                [2,"<tr><td>2/29/2016</td><td>Tong, Go</td><td>94</td><td>Just nothing</td><td>9400</td><td>000-1111-222</td></tr>"],
-                [2, "<tr><td>2/29/2016</td><td>Me, Haw</td><td>94</td><td>Just nothing</td><td>9400</td><td>000-1111-222</td></tr>"],
-                [3,"<tr><td>3/29/2016</td><td>Duran, Efren</td><td>94</td><td>Just nothing</td><td>9400</td><td>000-1111-222</td></tr>"],
-                [4,"<tr><td>4/29/2016</td><td>Suratos, Don</td><td>94</td><td>Just nothing</td><td>9400</td><td>000-1111-222</td></tr>"]
+var montharr = [[1, "<tr><td>1/29/2016</td><td>Jackson, Florida</td><td>001</td><td>Just nothing</td><td>9, 400</td><td>000-1111-222</td></tr>"],
+                [2,"<tr><td>2/29/2016</td><td>Tong, Go</td><td>002</td><td>Just nothing</td><td>10,000</td><td>000-2222-333</td></tr>"],
+                [2, "<tr><td>2/29/2016</td><td>Me, Haw</td><td>003</td><td>Just nothing</td><td>94,000</td><td>000-3333-444</td></tr>"],
+                [3,"<tr><td>3/29/2016</td><td>Duran, Efren</td><td>004</td><td>Just nothing</td><td>4,000</td><td>000-1111-222</td></tr>"],
+                [4,"<tr><td>4/29/2016</td><td>Suratos, Don</td><td>005</td><td>Just nothing</td><td>65,400</td><td>111-2222-333</td></tr>"]
                 ];
 
 function populateTable() {
@@ -20,7 +20,7 @@ var main = function() {
     populateTable();
     
     $('.select').change(function() {
-        //clear table
+        //clear table every change of option
         $('#depositTable tbody > tr').empty();
 
         var option = $( ".select option:selected" ).text();
